ce6a3d4f6279d13d05cd18c33d12b661b4557ee1
EduardIskhakov <edik.ru222@gmail.com>
2026-01-03 16:09:10 +0300

    L1.18

--- L1.18/main.go
+++ L1.18/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+func add(score *int, mu *sync.Mutex, wg *sync.WaitGroup, i int) {
+	defer wg.Done()
+	fmt.Println("Начало работы горутины", i)
+	time.Sleep(2 * time.Second)
+	mu.Lock()
+	*score += 1
+	mu.Unlock()
+	time.Sleep(time.Second)
+	fmt.Println("Завершнеие горутины", i)
+
+}
+
+func main() {
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+	score := 0
+	for i := range 4 {
+		wg.Add(1)
+		go add(&score, &mu, &wg, i)
+
+	}
+	wg.Wait()
+	fmt.Println(score)
 }
